ec403db238d9416998b621aab90aa0e1e17da3d8
ManshuTyagi <himanshut567@gmail.com>
2021-11-12 02:11:47 +0530

    RMX3031: Introduce Doze Overlay * Move Doze config from overlay to rro_overlay
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>
    Change-Id: Id666e6aeb2926ef580287feaa46cce14cc6ddc14
    Signed-off-by: TheSanty <sudhiryadav.igi@gmail.com>

--- device.mk
+++ device.mk
@@ -124,4 +124,6 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 # Wi-Fi
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay \
-    WifiOverlay
+    WifiOverlay \
+    DozeOverlaySystem \
+    DozeOverlaySystemUI
