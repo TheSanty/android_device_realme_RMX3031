9de9973760574d7a1ac66b0db9e42f69645e72bd
Kshitij Gupta <kshitijgm@gmail.com>
2021-11-12 02:48:01 +0530

    RMX3031: Address missing ImsServiceBase class * Android 11 has deprecated this class. * This is needed by Android 10 IMS stack. * Convert this into a boot JAR that will be available to system globally.
    Co-authored-by: Gagan Malvi <malvi@aospa.co>
    Signed-off-by: Gagan Malvi <malvi@aospa.co>
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>
    Change-Id: Ia685e2e10650c34b413c86fb19dd845baa8fa6c8
    Signed-off-by: TheSanty <sudhiryadav.igi@gmail.com>

--- device.mk
+++ device.mk
@@ -121,6 +121,10 @@ PRODUCT_SOONG_NAMESPACES += \
 -include $(DEVICE_PATH)/system_prop.mk
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
+# Symbols
+PRODUCT_PACKAGES += \
+    libshim_vtservice
+
 # Telephony
 PRODUCT_BOOT_JARS += \
     mediatek-common \
@@ -130,6 +134,9 @@ PRODUCT_BOOT_JARS += \
     mediatek-telecom-common \
     mediatek-telephony-base \
     mediatek-telephony-common
+
+PRODUCT_PACKAGES += \
+    ImsServiceBase
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
